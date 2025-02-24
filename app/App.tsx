5e3746127cc5a3a26434d0d82a4b7a4935c55951
Calixte Williams <calixte.williams@gmail.com>
2025-02-24 11:06:29 -0400

    Implement React Native Track Player

--- app/App.tsx
+++ app/App.tsx
@@ -1,20 +1,57 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState, useEffect} from 'react';
+import {
+  ActivityIndicator,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
-import React from 'react';
-import {Text, View} from 'react-native';
+import {setupPlayer, addTrack} from '../musicPlayerServices';
+import MusicPlayer from './screens/MusicPlayer';
 import '../global.css';
 
-function App() {
+function App(): React.JSX.Element {
+  const [isPlayerReady, setIsPlayerReady] = useState(false);
+
+  async function setup() {
+    let isSetup = await setupPlayer();
+
+    if (isSetup) {
+      await addTrack();
+    }
+
+    setIsPlayerReady(isSetup);
+  }
+
+  useEffect(() => {
+    setup();
+  }, []);
+
+  if (!isPlayerReady) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    );
+  }
+
   return (
     <View className="flex-1 bg-green-300 justify-center items-center">
       <Text className="text-lg font-bold">Grupo Radar Radio</Text>
+      <View style={styles.container}>
+        <StatusBar barStyle={'light-content'} />
+        <MusicPlayer />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
---
+++ app/radioStreams.ts
@@ -0,0 +1,57 @@
+import {Track} from 'react-native-track-player';
+
+export const playListData: Track[] = [
+  {
+    id: 1,
+    title: 'Radar 107.5',
+    artist: 'Grupo Radar',
+    artwork:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6sX4gCdX4C-_woD0GurvQrmN3YFTQt3FDCw&s',
+    url: 'https://radios.radiohd.com.mx/8268/stream',
+  },
+  {
+    id: 2,
+    title: 'Crystal 101.1',
+    artist: 'Grupo Radar',
+    artwork:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSv4O0G92LSXN1RVlvkOQP_BsvNieYgB9ofuA&s',
+    url: 'https://radios.radiohd.com.mx/8274/stream',
+  },
+  {
+    id: 3,
+    title: 'Radio Lobo Bajío 88.1 FM & 920 AM (Celaya)',
+    artist: 'Grupo Radar',
+    artwork:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTovz2ds7TObrgMLp9Gz-c5bckVYsckzOxeJQ&s',
+    url: 'https://radios.radiohd.com.mx/8188/stream',
+  },
+  {
+    id: 4,
+    title: 'Radar 88.9 FM (León)',
+    artist: 'Grupo Radar',
+    artwork: 'https://i.iheart.com/v3/re/new_assets/631fb7fbbd946d644930c789',
+    url: 'https://radios.radiohd.com.mx/8382/stream',
+  },
+  {
+    id: 5,
+    title: 'El y Ella 103.7 FM (Celaya)',
+    artist: 'Grupo Radar',
+    artwork: 'https://i.iheart.com/v3/re/new_assets/624f5fa90315729495f3b501',
+    url: 'https://radios.radiohd.com.mx/8272/stream',
+  },
+  {
+    id: 6,
+    title: 'Stereo Cristal 96.7  (Celaya)',
+    artist: 'Grupo Radar',
+    artwork:
+      'https://i.iheart.com/v3/re/assets.streams/65c430f7e0d5b6bdb0583cf5',
+    url: 'https://radios.radiohd.com.mx/8264/stream',
+  },
+  {
+    id: 7,
+    title: 'Radio Lobo 88.3 FM (San José Iturbide)',
+    artist: 'Grupo Radar',
+    artwork: 'https://i.iheart.com/v3/re/new_assets/636aa36d3aa70c4aee5d5106',
+    url: 'https://radios.radiohd.com.mx/8270/stream',
+  },
+];
