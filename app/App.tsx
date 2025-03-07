2b6b7b1562bb640a66282d20ee435e9bd648098e
Terrel-Deligny <terreldeligny@gmail.com>
2025-03-07 11:50:40 -0500

    globalcss not working, removed for now, basic carousel working currently without globlcss

--- app/App.tsx
+++ app/App.tsx
@@ -6,17 +6,12 @@
  */
 
 import React from 'react';
-import {Text, View} from 'react-native';
-import '../global.css';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import CarouselPoster from './components/carouselPoster';
 
 function App() {
   return (
     <GestureHandlerRootView>
-      <View className="flex-1 bg-green-300 justify-center items-center">
-        <Text className="text-lg font-bold">Grupo Radar Radio</Text>
-      </View>
       <CarouselPoster />
     </GestureHandlerRootView>
   );
