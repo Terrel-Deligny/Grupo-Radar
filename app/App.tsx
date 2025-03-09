fca3ef4aef42327ba6c2271cb6f7c29add9f4e27
Calixte Williams <calixte.williams@gmail.com>
2025-03-09 08:25:38 -0400

    Moved Capabilities back to app.tsx as it doesn't work in the services file

--- app/App.tsx
+++ app/App.tsx
@@ -4,6 +4,10 @@ import {ActivityIndicator, SafeAreaView, View} from 'react-native';
 import {setupPlayer, addTrack} from './services/musicPlayerServices';
 import MusicPlayer from './screens/MusicPlayer';
 import '../global.css';
+import TrackPlayer, {
+  Capability,
+  AppKilledPlaybackBehavior,
+} from 'react-native-track-player';
 //import TrackPlayer from 'react-native-track-player';
 
 function App(): React.JSX.Element {
@@ -13,6 +17,41 @@ function App(): React.JSX.Element {
     let isSetup = await setupPlayer();
     console.log(isSetup);
     if (isSetup) {
+      await TrackPlayer.updateOptions({
+        android: {
+          /**  This is the default behavior when stopping the app in the recents menu for android.
+           * ContinuePlayback (default)
+              This option will continue playing audio in the background when the app is removed from recents. The notification remains. This is the default.
+
+            PausePlayback
+              This option will pause playing audio in the background when the app is removed from recents. The notification remains and can be used to resume playback.
+
+            StopPlaybackAndRemoveNotification
+              This option will stop playing audio in the background when the app is removed from recents. The notification is removed and can't be used to resume playback. Users would need to open the app again to start playing audio.
+           */
+          appKilledPlaybackBehavior:
+            AppKilledPlaybackBehavior.StopPlaybackAndRemoveNotification,
+        },
+        capabilities: [
+          Capability.Play,
+          Capability.Pause,
+          Capability.SkipToNext,
+          Capability.SkipToPrevious,
+        ],
+
+        // Capabilities that will show up when the notification is in the compact form on Android
+        compactCapabilities: [Capability.Play, Capability.Pause],
+
+        /*
+        notificationCapabilities: [
+          Capability.Play,
+          Capability.Pause,
+          Capability.SkipToNext,
+          Capability.SkipToPrevious,
+          Capability.Stop,
+        ],
+        */
+      });
       await addTrack();
     }
     setIsPlayerReady(isSetup);
--- app/services/musicPlayerServices.js
+++ app/services/musicPlayerServices.js
@@ -1,9 +1,4 @@
-import TrackPlayer, {
-  Capability,
-  AppKilledPlaybackBehavior,
-  Event,
-  RepeatMode,
-} from 'react-native-track-player';
+import TrackPlayer, {Event, RepeatMode} from 'react-native-track-player';
 
 import {playListData} from '../assets/data/radioStreams';
 
@@ -43,46 +38,7 @@ export async function playbackService() {
     TrackPlayer.skipToPrevious();
   });
   TrackPlayer.addEventListener(Event.RemoteStop, () => {
+    console.log('Event.RemoteStop');
     TrackPlayer.stop();
-  });
-
-  await TrackPlayer.updateOptions({
-    android: {
-      /**  This is the default behavior when stopping the app in the recents menu for android.
-       * ContinuePlayback (default)
-          This option will continue playing audio in the background when the app is removed from recents. The notification remains. This is the default.
-
-        PausePlayback
-          This option will pause playing audio in the background when the app is removed from recents. The notification remains and can be used to resume playback.
-
-        StopPlaybackAndRemoveNotification
-          This option will stop playing audio in the background when the app is removed from recents. The notification is removed and can't be used to resume playback. Users would need to open the app again to start playing audio.
-       */
-      appKilledPlaybackBehavior:
-        AppKilledPlaybackBehavior.StopPlaybackAndRemoveNotification,
-    },
-    capabilities: [
-      Capability.Play,
-      Capability.Pause,
-      Capability.SkipToNext,
-      Capability.SkipToPrevious,
-      Capability.Stop,
-    ],
-
-    // Capabilities that will show up when the notification is in the compact form on Android
-    compactCapabilities: [
-      Capability.Play,
-      Capability.Pause,
-      Capability.SkipToNext,
-      Capability.SkipToPrevious,
-    ],
-
-    notificationCapabilities: [
-      Capability.Play,
-      Capability.Pause,
-      Capability.SkipToNext,
-      Capability.SkipToPrevious,
-      Capability.Stop,
-    ],
   });
 }
