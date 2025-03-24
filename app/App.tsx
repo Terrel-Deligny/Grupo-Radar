e4b3e19a82c18c72ff26d9469771b7fc55c9eb41
Terrel Deligny <terreldeligny@gmail.com>
2025-03-24 17:57:46 -0400

    HomeScreen changes to code
    -Added pagination dots to CarouselPoster
    -Fixed styling/sizing of carousels
    -Downloaded navigation dependencies for stack navigation
    -Created a HomeScreen in Screens to call CarouselPoster and CarouselNews

--- app/App.tsx
+++ app/App.tsx
@@ -1,25 +1,30 @@
 import React, {useState, useEffect} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 import {ActivityIndicator, SafeAreaView, StyleSheet, View} from 'react-native';
-
+//import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {addTrack} from './services/AudioPlayerServices';
 import {SetupService} from './services/SetupService';
 import MusicPlayer from './screens/MusicPlayer';
 import '../global.css';
 import TrackPlayer from 'react-native-track-player';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import CarouselPoster from './components/CarouselPoster';
-import CarouselNews from './components/CarouselNews';
-import {WixProvider} from '@wix/sdk-react';
+//import CarouselPoster from './components/CarouselPoster';
+//import CarouselNews from './components/CarouselNews';
+//import {WixProvider} from '@wix/sdk-react';
+import HomeScreen from './screens/HomeScreen';
+
+//const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <GestureHandlerRootView className="flex-1">
-      <WixProvider>
-        <CarouselPoster />
-        <CarouselNews />
-        <Inner />
-      </WixProvider>
-    </GestureHandlerRootView>
+    <NavigationContainer>
+      {
+        <GestureHandlerRootView className="flex-1">
+          <HomeScreen />
+          <Inner />
+        </GestureHandlerRootView>
+      }
+    </NavigationContainer>
   );
 }
 
